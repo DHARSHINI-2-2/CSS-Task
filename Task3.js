2eff27e5e2a62655c57ff3f49e7bcbb64d7cf21b
DHARSHINI-2-2 <smdharshini22@gmail.com>
2025-07-20 22:18:49 +0530

    home task 3

---
+++ Task3.js
@@ -0,0 +1,16 @@
+//break statement
+for(let i=10;i>=1;i--){
+    if(i==5){
+        break;
+    }
+    console.log(i);
+}
+
+
+//continue statement
+for(let i=0;i<=10;i++){
+    if(i%2==0){
+        continue;  
+    }
+     console.log(i);
+}
