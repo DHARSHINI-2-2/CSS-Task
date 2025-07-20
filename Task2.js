2285b4f55cbb49b8d3954388ed5c61c6bd9985ae
DHARSHINI-2-2 <smdharshini22@gmail.com>
2025-07-20 22:18:16 +0530

    home task 2

---
+++ Task2.js
@@ -0,0 +1,41 @@
+//while loop
+  let i=1;
+  while(i<=10){
+      if(i%2==0){
+          console.log(i);
+       
+      }
+       i++;    
+ }
+
+
+
+
+//do-while loop
+console.log("do while loop")
+let x=1;
+do{
+         console.log(x);
+         x++;
+}while(x<10);
+
+
+
+
+//for loop
+ console.log("for loop")
+ for(let i=1;i<=5;i++){
+         console.log(i);
+ }
+
+
+
+
+//nested for loop
+console.log("nested for loop")
+for(let i=1;i<=2;i++){
+        for(let j=1;j<=10;j++){
+                console.log(i+"*"+j+"="+(i*j));
+        }
+}
+
